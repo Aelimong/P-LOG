df2a9ff8097f9af9d8aafc4fa78b0ea46a2002d1
Aelimong <dofla0507@gmail.com>
2023-07-17 21:14:34 +0900

    2023-07-17 21:14

--- saint-james-iced-teas/js/index.js
+++ saint-james-iced-teas/js/index.js
@@ -34,13 +34,53 @@ document.addEventListener("mousemove", (e) => {
   // const circleDiv = document.createElement("div");
   // circleDiv.classList.add("circle");
   // circleDiv.innerText = "CLICK";
-  console.log(pageY); //계
-  console.log(navHeight); //78
+  //console.log(pageY); //계
+  //console.log(navHeight); //78
   // 2. append를 한 번만...??
   //메인 섹션 안에서만 보이고
   if (pageY >= navHeight && pageY <= mainSecHeight) {
-    circle.style.display = "flex";
+    circle.style.opacity = "1";
   } else {
-    circle.style.display = "none";
+    // circle.style.scale = "1.1";
+    circle.style.opacity = "0";
   }
 });
+
+let slideArr = document.getElementsByClassName("slides");
+let mainSlide = document.getElementById("main-slide");
+let slideX = 0;
+
+let viewWidth = window.innerWidth;
+
+//클릭하면 이미지 옆으로 슬라이드@!!!
+function slideMainImages() {
+  // for (let i = 0; i < slideArr.length; i++) {
+  //   slideX += slideArr[i].width;
+  //   slideArr[i].setAttribute(
+  //     "style",
+  //     `transform: translate3d(-${slideX}px, 0, 0px);`
+  //   );
+  //   console.log(slideArr[i].getAttribute("style"));
+  //   //왼쪽으로 이동한 화면 크기가 뷰포트 너비를 넘어갈 때 0으로 초기화
+  //   if (slideX > viewWidth) {
+  //     slideArr[i].setAttribute("style", `transform: translate3d(0px, 0, 0px);`);
+  //     slideX = 0;
+  //   }
+  // }
+}
+
+let colorArr = ["red", "blue", "green", "pink"];
+let colorIndex = 0;
+
+//섹션 배경 색깔 변경!!
+function changeBackgroundColor(currentIndex) {
+  //background-color: ${colorArr[colorIndex]}
+
+  mainSlide.classList.toggle(colorArr[colorIndex]);
+
+  colorIndex++;
+
+  if (colorIndex === colorArr.length + 1) {
+    colorIndex = 0;
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,8 @@
+let slideArr = document.getElementsByClassName("slides");
+let mainSlide = document.getElementById("main-slide");
+let slideX = 0;
+
+let viewWidth = window.innerWidth;
+
+//클릭하면 이미지 옆으로 슬라이드@!!!
+function slideMainImages() {}
